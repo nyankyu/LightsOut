2e1dfe40c24bb2201c2db2ccb6ee03dbfb8e6af2
nao <fukujinn@gmail.com>
2015-10-12 14:25:07 +0900

    implement Board Class.

--- app/src/main/java/jp/gr/java_conf/kobitokaba/lightsout/Board.java
+++ app/src/main/java/jp/gr/java_conf/kobitokaba/lightsout/Board.java
@@ -1,51 +1,39 @@
 package jp.gr.java_conf.kobitokaba.lightsout;
 
 import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.drawable.Drawable;
+import android.support.percent.PercentRelativeLayout;
+import android.text.TextPaint;
 import android.util.AttributeSet;
 import android.view.View;
 
-public class Cell extends View {
+public class Board extends PercentRelativeLayout {
 
-    private CellState mState;
-
-    public Cell(Context context) {
+    public Board(Context context) {
         super(context);
         init(null, 0);
     }
 
-    public Cell(Context context, AttributeSet attrs) {
+    public Board(Context context, AttributeSet attrs) {
         super(context, attrs);
         init(attrs, 0);
     }
 
-    public Cell(Context context, AttributeSet attrs, int defStyle) {
+    public Board(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
         init(attrs, defStyle);
     }
 
     private void init(AttributeSet attrs, int defStyle) {
-        CellState.setState(this, CellState.GRAY);
-        setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                toggle();
-            }
-        });
-    }
-
-    void setState(CellState newState) {
-        mState = newState;
-    }
-
-    void toggle() {
-        mState.changeNext(this);
     }
 
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
         setMeasuredDimension(getDefaultSize(getSuggestedMinimumWidth(), widthMeasureSpec),
                 getDefaultSize(getSuggestedMinimumWidth(), widthMeasureSpec));
-
-        //super.onMeasure(widthMeasureSpec, heightMeasureSpec);
     }
 }
